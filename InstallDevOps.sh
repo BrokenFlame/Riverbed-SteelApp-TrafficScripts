274fdd736895e4ff0ddebf1f069638860031ed25
Tr <tr.thambi@gmail.com>
2015-01-05 13:17:32 +0000

    Initial Script to install RiverBed SteelApp

---
+++ InstallDevOps.sh
@@ -0,0 +1,7 @@
+wget https://support.riverbed.com/bin/support/download?sid=p2rmalibflneacije5b4sdac2q
+mv download\?sid\=p2rmalibflneacije5b4sdac2q SteelApp.tzg
+tar -xzvf SteelApp.tzg
+sudo mv ZeusTM_98r2_Linux-x86_64 Zeus /Zeus
+cd /Zeus
+sudo bash zinstall
+
